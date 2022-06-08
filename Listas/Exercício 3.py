312206f4302d8eb36ea516ba3aba81d2b4f01a3e
paris <pariselucas03@gmail.com>
2022-06-08 01:00:18 -0300

    Exercise 3

--- Listas/Exercício 3.py
+++ Listas/Exercício 3.py
@@ -1,2 +1,15 @@
 #Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
 
+notas = [0, 0, 0, 0, 0]
+x = 0
+soma = 0
+
+while x < 5:
+    notas[x] = float(input(f"Nota{x}: "))
+    soma += notas[x]
+    x += 1
+x = 0
+while x < 5:
+    print(f"Notas{[x]}: {notas[x]} ")
+    x += 1
+print(f"Média final: {soma / x}")
